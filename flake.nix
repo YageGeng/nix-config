0de92053dbe8deafb4c8e2cb945d30a16513a734
GengYage <icoderdev@gmail.com>
2025-02-28 17:10:07 +0800

    init: hello nixos

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "isbest's NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+	home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.isbest = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
